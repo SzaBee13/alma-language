6ab23b1d7df524c4aa4cc3147a4200f9e31421af
SzaBee13 <miabajodlol@gmail.com>
2025-01-12 08:49:14 +0100

    version 1.2

--- alma.cmd
+++ alma.cmd
@@ -3,27 +3,23 @@ set VERSION=1.2
 
 if "%1"=="-v" (
     echo Alma Language v%VERSION%
-    pause
     exit /b
 )
 
 if "%1"=="" (
     echo Error: Specify an .alma file to run.
-    pause
     exit /b
 )
 
 python --version >nul 2>&1
 if %ERRORLEVEL% neq 0 (
     echo Python is not installed. Visit: https://www.python.org
-    pause
     exit /b
 )
 
 node --version >nul 2>&1
 if %ERRORLEVEL% neq 0 (
     echo Node.js is not installed. Visit: https://nodejs.org
-    pause
     exit /b
 )
 
--- run_alma.bat
+++ run_alma.bat
@@ -3,21 +3,18 @@
 python --version >nul 2>&1
 if %ERRORLEVEL% neq 0 (
     echo Python is not installed. Visit: https://www.python.org
-    pause
     exit /b
 )
 
 node --version >nul 2>&1
 if %ERRORLEVEL% neq 0 (
     echo Node.js is not installed. Visit: https://nodejs.org
-    pause
     exit /b
 )
 
 if "%~1"=="" (
     echo Error: Enter an .alma file as an argument!
     echo Usage: run_alma.bat input.alma
-    pause
     exit /b
 )
 
@@ -28,7 +25,6 @@ set OUTPUT_FILE=temp_output.js
 python "%~dp0alma_transpiler.py" "%INPUT_FILE%" "%OUTPUT_FILE%"
 if not exist "%OUTPUT_FILE%" (
     echo Error: Compiler did not create %OUTPUT_FILE%.
-    pause
     exit /b
 )
 
@@ -36,7 +32,6 @@ node "%OUTPUT_FILE%"
 if %ERRORLEVEL% neq 0 (
     echo An error occurred while running Node.js.
     del "%OUTPUT_FILE%"
-    pause
     exit /b
 )
 
